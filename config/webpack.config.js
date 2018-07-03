c15451e2dabbb43f7089fac5aa010eca94dc9901
kongqingfei <kongqingfei@emar.com>
2018-07-03 16:08:32 +0800

    up

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const externalsDep = require('externals-dependencies')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const dest = '../dist'
+const src = '../src'
 
 module.exports = {
   entry: './src/server/app.js',
   target: 'node',
+  node: {__dirname: true},
   mode: 'development',
   output: {
     filename: 'app.js',
     path: path.resolve(__dirname, dest),
   },
   // externals: [externalsDep()],
+  // externals: ['process'],
+  plugins: [
+    new CopyWebpackPlugin([{from: path.resolve(__dirname, `${src}/client`), to: path.resolve(__dirname, dest)}]),
+  ],
 };
--- src/server/app.js
+++ src/server/app.js
@@ -1,14 +1,24 @@
 const Koa = require('koa')
+const server = require('koa-static')
 const childProcess = require('child_process')
+const path = require('path')
 
 const app = new Koa()
-app.use(async ctx => {
+app.use(async (ctx, next) => {
+  if (ctx.path.endsWith('html')) {
+    return next()
+  }
   const query = ctx.query || {}
   const {name, version} = query
   if (!name || !version) {
     return ctx.body = 'name and version is required'
   }
-  const ret = childProcess.execFileSync(`/data/voyager/git/${name}/rr.sh`, [version], {})
-  ctx.body = `部署结束\n\n\n${ret.toString()}`
+  try {
+    const ret = childProcess.execFileSync(`/data/voyager/git/${name}/rr.sh`, [version], {})
+    ctx.body = `部署结束\n\n\n${ret.toString()}`
+  } catch (e) {
+    ctx.body = `部署异常\n\n\n${e.stack}`
+  }
 })
+app.use(server(path.resolve(__dirname, global.process.env.NODE_ENV === 'development' ? '../client/' : '.')))
 app.listen(8000)
